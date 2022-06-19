628204bd18be42b1463730be417e9ea4cd0c0612
Zywilla Fechner <zwfdane@gmail.com>
2022-06-19 13:38:51 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+""" Napisz program, który:
+    Dla liczb z zakresu od 0 do 100, wyświetli te, które są podzielne przez 5.
+    W następnym wierszu wyświetli te liczby podniesione do potęgi 3.
+"""
+for number in range(0,101,5):
+    print(f"Liczba podzielna przez 5: {number}. \n Jej sześcian to: {number * number * number}.")
